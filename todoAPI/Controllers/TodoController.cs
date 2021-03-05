df45cfc2de8c69f6ed092ce818fe9578f600e23b
Yazılım 3 <Yazlm 3@DESKTOP-UTS65EK>
2021-03-05 09:10:06 +0300

    Add project files.

---
+++ todoAPI/Controllers/TodoController.cs
@@ -0,0 +1,57 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using todoAPI.Models;
+
+namespace todoAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using IDbConnection con = new SqlConnection(Program.ConnectionString);
+            return Ok(con.GetAll<Todo>());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using IDbConnection con = new SqlConnection(Program.ConnectionString);
+            return Ok(con.Get<Todo>(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Todo value)
+        {
+            using IDbConnection con = new SqlConnection(Program.ConnectionString);
+            return Ok(con.Insert(value));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Todo value)
+        {
+            using IDbConnection con = new SqlConnection(Program.ConnectionString);
+            return Ok(con.Update(value));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            using IDbConnection con = new SqlConnection(Program.ConnectionString);
+            return Ok(con.Delete(new Todo { ID = id }));
+        }
+        public static Todo GetTodo(this object o)
+        {
+
+            return new Todo();
+        }
+    }
+}
---
+++ todoAPI/Models/Todo.cs
@@ -0,0 +1,24 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoAPI.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public TodoStatus Status { get; set; }
+    }
+    public enum TodoStatus
+    {
+        Activate = 0,
+        Process = 1,
+        Complated = 2
+    }
+}
